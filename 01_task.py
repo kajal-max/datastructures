cab3514711066667ab32fd550a3fa5e23cff107b
kajal-max <kajal.mm@outlook.com>
2022-10-19 18:09:53 -0700

    status update

---
+++ 01_task.py
@@ -0,0 +1,78 @@
+# Question - 1
+Mylist = ["Python", 9, False, 9.9, 9j, "List", None, True, 26.9, 1+9j]
+for element in Mylist:
+    print(type(element))
+print(Mylist)
+
+      
+# Question - 2
+
+# a = ["ram", 10, "shyam", 12.5, True]
+
+# print(a)
+# print(a[:])
+# print(a[1:])
+# print(a[:4])
+# print(a[1:5])
+# print(a[-4:-1])
+# print(a[-4:])
+# print(a[:-1])
+
+# Question - 3
+# mylist = [5, 10, 15, 20,]
+# total = sum(mylist)
+# print("sum of all elements in given list:", total)
+
+# s=1
+# mylist = [2, 3, 4, 5]
+# for x in mylist:
+#     s = s*x
+# print(s)
+
+
+# Question - 4
+# mylist = [20, 100, 20, 1, 10]
+# print("smallest element is:", min(mylist))
+# print("largest element is:", max(mylist))
+
+
+# Question - 5
+# num = [1, 2, 3, 4, 7, 8, 9, 10]
+# num = [x for x in num if x%2 != 0]
+# print(num)
+
+
+# Question - 6
+# list6 = list ( )
+# for i in range (1,30):
+#     list6.append(i**2)
+# print(list6[:5]) 
+# print(list6[-5:])   
+
+
+# Question - 7
+# num1 = [1,3,5,7,9,10] 
+# num2 = [2,4,6,8]
+# num1[-1:] = num2
+# print(num1)
+
+
+# Question - 8
+# a={1:10,2:20} 
+# b={3:30,4:40}
+# a.update(b)
+# print(a)
+
+
+# Question - 9
+# n = int(input("enter a number: "))
+# d = {x:x*x for x in range (1,n+1)}
+# print (d)
+
+
+# Question - 10
+# values = input("input some comma seperated numbers:")
+# list = values.split(",")
+# tuple = tuple(list)
+# print("list:",list)
+# print("tuple:",tuple)
